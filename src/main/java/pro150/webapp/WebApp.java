7637ee10f7a051df4da4997db97cbb1cf82802d7
tikalaka <baydadan001@gmail.com>
2019-11-04 18:12:10 -0700

    added some test

--- src/main/java/pro150/webapp/WebApp.java
+++ src/main/java/pro150/webapp/WebApp.java
@@ -2,7 +2,12 @@ package pro150.webapp;
 
 public class WebApp {
 
+
     public static void main(String[] args) {
         System.out.println("First setup!");
+
+
+        System.out.println("something");
+
     }
 }
