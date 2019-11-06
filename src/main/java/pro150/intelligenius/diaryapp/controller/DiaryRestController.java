f671af82403d824addb425f4b7b69a33b20f4125
Searjasub Lopez <searjasub.lopez@outlook.com>
2019-11-05 21:38:42 -0700

    Added some mapping

--- src/main/java/pro150/intelligenius/diaryapp/controller/DiaryRestController.java
+++ src/main/java/pro150/intelligenius/diaryapp/controller/DiaryRestController.java
@@ -1,15 +1,36 @@
 package pro150.intelligenius.diaryapp.controller;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/diary")
 public class DiaryRestController {
 
+    @RequestMapping(path = "/", method = RequestMethod.POST)
+    public void doLogin(){
 
+    }
 
+    @RequestMapping(path = "/", method = RequestMethod.GET)
+    public void loginScreen(){
 
+    }
 
+    @RequestMapping(path = "/registration", method = RequestMethod.POST)
+    public void doRegistration(){
 
+    }
+
+    @RequestMapping(path = "/delete/{id}", method = RequestMethod.DELETE)
+    public void deleteEntryById(@PathVariable long id){
+
+    }
+
+    @RequestMapping(path = "/collection", method = RequestMethod.GET)
+    public void getAllEntries(){
+
+    }
 }
--- src/main/java/pro150/intelligenius/diaryapp/model/Entry.java
+++ src/main/java/pro150/intelligenius/diaryapp/model/Entry.java
@@ -1,4 +1,32 @@
 package pro150.intelligenius.diaryapp.model;
 
 public class Entry {
+
+    private String title;
+    private String content;
+    private String timeInMilliSeconds;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getTimeInMilliSeconds() {
+        return timeInMilliSeconds;
+    }
+
+    public void setTimeInMilliSeconds(String timeInMilliSeconds) {
+        this.timeInMilliSeconds = timeInMilliSeconds;
+    }
 }
