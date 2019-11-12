fd971ace5bf20dfa7796b48950b6a613af891e6c
nmolstad <nashmolstad@gmail.com>
2019-11-11 20:48:20 -0700

    updated .properties

--- src/main/java/pro150/intelligenius/diaryapp/controller/ProfileRestController.java
+++ src/main/java/pro150/intelligenius/diaryapp/controller/ProfileRestController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 import pro150.intelligenius.diaryapp.model.Profile;
 
 import java.util.List;
@@ -15,6 +16,13 @@ public class ProfileRestController {
 
     @Autowired
     private ProfileJpaRepository profileJpaRepository;
+
+    @RequestMapping(path = "/{name}", method = RequestMethod.GET)
+    public ModelAndView home(@PathVariable String name){
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("hello");
+        return mav;
+    }
 
     @RequestMapping(path = "/{name}/{dateOfBirth}", method = RequestMethod.POST)
     public int createStudent(@PathVariable String name, @PathVariable String dateOfBirth) {
