74c07a8ddbaf80d80945944178014008869ac059
tikalaka <baydadan001@gmail.com>
2019-11-20 17:28:32 -0700

    principal added

--- src/main/java/pro150/intelligenius/diaryapp/controller/EntryRestController.java
+++ src/main/java/pro150/intelligenius/diaryapp/controller/EntryRestController.java
@@ -1,11 +1,13 @@
 package pro150.intelligenius.diaryapp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import java.security.Principal;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import pro150.intelligenius.diaryapp.model.Entry;
+import pro150.intelligenius.diaryapp.model.User;
 
 import java.util.List;
 
@@ -16,12 +18,14 @@ public class EntryRestController {
     @Autowired
     private EntryJpaRepository entryJpaRepository;
 
-//    @Autowired
-//    private UserJpaRepository userJpaRepository;
+    @Autowired
+    private UserJpaRepository userJpaRepository;
 
     //TODO, This line may need changing, not sure of the path
     @RequestMapping(path = "", method = RequestMethod.POST)
-    public void createEntry(@RequestBody Entry newEntry) {
+    public void createEntry(@RequestBody Entry newEntry,Principal principal) {
+        User u = userJpaRepository.findById(principal.getName()).orElse(null);
+
         entryJpaRepository.save(newEntry);
     }
 
